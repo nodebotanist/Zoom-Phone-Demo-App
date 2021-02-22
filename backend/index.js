52965aa4e216c5fc66ab17998e77a2ad6079ba7d
Kassian Wren <kassian.wren@zoom.us>
2021-02-22 14:32:10 -0600

    Auth complete-- redirects to frontend

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,9 @@
 const express = require('express')
+const session = require('express-session')
 const bodyParser = require('body-parser')
 const dotenv = require('dotenv')
 
-const authRoutes = require('./routes/auth')
+const auth = require('./routes/auth')
 
 dotenv.config()
 
@@ -11,12 +12,17 @@ const port = 8000
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({
+  secret: "336565067d7ff0e939b7dfc53323f51e",
+  cookie: {},
+  maxAge: 60000
+}))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/auth', authRoutes.completeAuth)
+app.get('/auth', auth.completeAuth)
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,16 +3,28 @@ const axios = require('axios')
 module.exports = {
     completeAuth: async function(req, res) {
         const code = req.query.code
-        console.log(code)
         const keyBuffer = Buffer.from(`${process.env.ZOOM_CLIENT_ID}:${process.env.ZOOM_CLIENT_SECRET}`)
         let accessToken = await axios({
             url: `https://zoom.us/oauth/token?grant_type=authorization_code&code=${code}&redirect_uri=${process.env.ZOOM_REDIRECT_URI}`,
-            method: 'post',
+            method: 'POST',
             headers: {
                 Authorization: `Basic ${keyBuffer.toString('base64')}` 
             }
         })
-        console.log(accessToken.data)
-        res.send('success')
+        let userData = await axios({
+            url: `https://api.zoom.us/v2/users/me`,
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${accessToken.data.access_token}`
+            }
+        })
+        console.log(userData.data)
+        //save user data to the session
+        req.session.userData = {
+            userId: userData.data.id,
+            accessToken: accessToken.data.access_token,
+            refreshToken: accessToken.data.refresh_token
+        }
+        res.redirect('http://localhost:3000')
     }
 }
