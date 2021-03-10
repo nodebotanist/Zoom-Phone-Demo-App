73c2a05f3a3c2b324ae0d5f0fc00173f69beb470
Kassian Wren <kassian.wren@zoom.us>
2021-03-10 13:33:09 -0600

    Updated profile component

--- frontend/src/call/profile.js
+++ frontend/src/call/profile.js
@@ -1,15 +1,24 @@
-function Profile() {        
-        const userProfile = fetch('https://nodebotanist.ngrok.io/profile', {
-        method: 'GET'
-    }).then(async function(userProfile){
-        userProfile = await userProfile.text()
-        console.log(userProfile)    
-    })
+import React, {useState, useEffect} from 'react'
+
+const Profile = () => {
+    const [profile, setProfile] = useState([])
+
+    useEffect(() => {
+        fetch('https://nodebotanist.ngrok.io/profile', {
+            method: 'GET'
+        }).then(async function(userProfile){
+            userProfile = await userProfile.json()
+            setProfile(userProfile)
+            console.log(userProfile) 
+        })
+    }, [])
+
     return (
         <div className="App col col-lg-3">
-        <h1>User Profile:</h1>
+            <h1>User Profile:</h1>
+            <p>Email: {profile ? profile.email: 'no user'}</p>
         </div>
-    );
+    );    
 }
   
   export default Profile;
